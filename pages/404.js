192230a20ae47126ba65bcab0ba94bf1fbe11376
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-04-08 21:35:54 -0300

    Create a Link Component

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from "../src/components/Link";
+
+export default function Page404(){
+  return (
+    <div>
+      <h1>Você se perdeu e caiu na pagina 404</h1>
+      <Link href="/">
+        Ir para home
+      </Link>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from "../src/components/Link";
+
 export default function HomePage(){
   return (
     <div>
       <h1>NextJS Course</h1>
+      <Link href="/faq">
+        Ir para a FAQ
+      </Link>
     </div>
   )
 }
