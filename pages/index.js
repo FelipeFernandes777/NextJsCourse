d2044a93035eda8e13be161d15483e60cf6bfd93
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-04-08 22:34:05 -0300

    CSS in JS

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
+import React from "react";
 import Link from "../src/components/Link";
+
+function Title({children,as}) {
+  const Tag = as;
+  return (
+    <React.Fragment>
+      <Tag>{children}</Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: blue;
+          font-family: sans-serif;
+        }
+       `}</style>
+    </React.Fragment>
+  )
+}
 
 export default function HomePage(){
   return (
     <div>
-      <h1>NextJS Course</h1>
+      <Title as="h2">NextJS Course</Title>
       <Link href="/faq">
         Ir para a FAQ
       </Link>
---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,14 @@
+function GlobalStyle() {
+  return (
+    <style jsx global>{`
+      body {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: sans-serif;
+      }
+    `}</style>
+  )
+}
+
+export default GlobalStyle;
